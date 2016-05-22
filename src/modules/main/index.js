6d0d14fcaf1bd0599b9ba75e3c9bb6514e9540e3
vs-zhang <vincentzhang1209@gmail.com>
2016-05-22 01:54:33 -0400

    fix Hot module replacement(HMR) not working with new module

--- src/modules/main/index.js
+++ src/modules/main/index.js
@@ -2,14 +2,23 @@ import React from 'react'
 import * as todos from '../todos'
 import { combineReducers } from 'redux'
 
-export const reducer = combineReducers({
+if (module.hot) {
+  module.hot.accept()
+}
+
+const reducer = combineReducers({
   [todos.name]: todos.reducer
 });
 
+class Container extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Todos app</h3>
+        <todos.TodoListComponent />
+      </div>
+    )
+  }
+}
 
-export const Container = () => (
-  <div>
-    <h3>Todo app aefa</h3>
-    <todos.TodoListComponent />
-  </div>
-);
+export {reducer, Container}
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
 
-var devFlagPlugin = new webpack.DefinePlugin({
-  __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
-});
-
 module.exports = {
   devtool: 'source-map',
   entry: [
