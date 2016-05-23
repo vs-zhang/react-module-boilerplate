79dcbb4adbf0c330c20b807ebc9bf52300f19cea
vs-zhang <vincentzhang1209@gmail.com>
2016-05-23 01:16:06 -0400

    rename Root.js to index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as todos from './modules/todos'
 import { Provider } from 'react-redux'
-import RootComponent from './modules/Root/Root'
+import Root from './modules/Root/index'
 import { createStore, applyMiddleware } from 'redux'
 import configureStore from './utils/store/configureStore'
 
@@ -10,9 +10,7 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>
-      <RootComponent />
-    </div>
+    <Root />
   </Provider>,
   document.getElementById('app')
 );
--- src/modules/Root/index.dev.js
+++ src/modules/Root/index.dev.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { DevTools } from '../../utils/devtools'
 import * as main from '../main'
 
-export default class RootComponent extends Component {
+export default class Root extends Component {
   render() {
     return (
       <div>
         <main.Container />
+        <DevTools />
       </div>
     )
   }
 }
+
+
