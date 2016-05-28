f7db254311b7c871cd8ebb6e8caf452753f1744c
vs-zhang <vincentzhang1209@gmail.com>
2016-05-27 21:06:24 -0400

    update some package

--- src/components/todoList/actions.js
+++ src/components/todoList/actions.js
@@ -1,8 +1,5 @@
 import { name } from './__init__'
 
 export const ADD = `${name}/ADD`;
-export const DELETE = `${name}/DELETE`;
 
-export const add = text => ({ type: ADD, text: text });
-
-
+export const add = (text) => ({ type: ADD, text: text });
